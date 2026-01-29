ef108438fcfeb086f0329613ed2e48a34e9742da
JoJoJoDoDoDo <itsoref2028@kehillahstudent.org>
2026-01-29 08:45:58 -0800

    ye

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 song = "I like to eat, eat, eat, apples and bananas"
 
 def sing(vowel):
-    # your code here
+    print(song.replace("a", "ee"))
+    
 
 sing("ay")
 sing("ee")
 sing("i")
 sing("o")
 sing("u")
+
+
+
+a = "I like to eat, eat, eat, apples and bananas"
+b = a.replace("a","aaa")
+c = b.replace("I","III")
+d = c.replace("k","kkk")
+e = d.replace("l","lll")
+f = e.replace("e","eee")
+print(f)
+
+
+def sing(vowel):
+    print(song.replace("a", "aaa"))
+    
